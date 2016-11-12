717157b61369ef58ea47ac657d4c03f947fe9ecb
caschotch <caschotchii@gmail.com>
2016-11-12 20:14:50 +0000

    Completed CP 27 Posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,36 +8,52 @@
 
 require 'random_data'
 
-#create topics
+#Create Users
+5.times do
+    User.create!(
+        name:           RandomData.random_name,
+        email:          RandomData.random_email,
+        password:       RandomData.random_sentence
+        )
+    end
+    users = User.all
+
+#Create topics
 15.times do
     Topic.create!(
-        name: RandomData.random_sentence,
-        description: RandomData.random_paragraph
+        name:           RandomData.random_sentence,
+        description:    RandomData.random_paragraph
         )
 end
-
 topics = Topic.all
-
 
 #Create Posts
 50.times do
-    
     Post.create!(
-        topic: topics.sample,     
-        title: RandomData.random_sentence,
-        body:  RandomData.random_paragraph
+        user:           users.sample,
+        topic:          topics.sample,     
+        title:          RandomData.random_sentence,
+        body:           RandomData.random_paragraph
     )
 end
 posts = Post.all
 
+#Create comments
 100.times do
     Comment.create!(
-        post: posts.sample,
-        body: RandomData.random_paragraph
+        post:           posts.sample,
+        body:           RandomData.random_paragraph
     )
 end
 
+user = User.first
+user.update_attributes!(
+    email: 'caschotchii@gmail.com',
+    password: 'helloworld'
+    )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
